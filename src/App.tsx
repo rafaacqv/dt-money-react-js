2e2b9ac256395969393830b1d96f79d4d84ad8f1
Rafael Araujo <rafaacq@gmail.com>
2021-04-01 00:07:26 -0300

    Add list item feature and TransactionContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
+
 import { GlobalStyle } from "./styles/global";
 
 
@@ -21,14 +23,17 @@ export function App() {
   
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      
       <Dashboard />
+      
       <NewTransactionModal 
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
       />
+      
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
